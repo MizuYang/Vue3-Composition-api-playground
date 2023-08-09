3b5a5e5b7205a0d2c815cf9fbc6e636b631386a5
Mizu <psdk55052004@gmail.com>
2023-08-09 12:04:20 +0800

    修改 儲存庫名稱

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue3-cli-template/' //* 儲存庫名稱
+    ? '/Vue3-Composition-api-playground/' //* 儲存庫名稱
     : '/'
 })
