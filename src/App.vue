05f11a4951ae47cab2e0dae9f26aaf8d32dc3089
Mizu <psdk55052004@gmail.com>
2023-08-23 12:00:27 +0800

    修改 主題色: 於 App.vue 統一設定

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,20 @@
        style="height: 45px;line-height:1.5;">
        <NavbarList />
   </nav>
-  <router-view class="mt-5" style="width:100vw;" />
+  <router-view class="mt-5" :data-theme="theme" style="width:100vw;" />
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { toRefs } from 'vue'
 import { useStore } from 'vuex'
 import NavbarList from '@/components/layout/NavbarList.vue'
 
 const store = useStore()
-const getTheme = () => store.dispatch('theme/getTheme')
-const getBodyBgColor = () => store.dispatch('theme/getBodyBgColor')
+const { dispatch } = store
+dispatch('theme/getTheme')
+dispatch('theme/getBodyBgColor')
 
-getTheme()
-
-onMounted(() => {
-  getBodyBgColor()
-})
+const { theme } = toRefs(store.state.theme)
 </script>
 
 <style lang="scss">
