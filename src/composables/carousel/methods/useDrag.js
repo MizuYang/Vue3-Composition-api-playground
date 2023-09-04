d4e3d2fe76015cee078236122995f7378bf5e32b
Mizu <psdk55052004@gmail.com>
2023-09-04 15:53:04 +0800

    修改 輪播: 將函式拉出來

--- src/composables/carousel/methods/useDrag.js
+++ src/composables/carousel/methods/useDrag.js
@@ -9,25 +9,31 @@ export function useDrag () {
     upPosition: 0
   })
 
+  let myStoreName = ''
+
   function getDownPosition (e, storeName) {
     states.downPosition = e.pageX
     commit(`${storeName}/STOP_PLAY`)
 
+    myStoreName = storeName
+
     document.body.addEventListener('mouseup', getUpPosition)
-    function getUpPosition (e) {
-      states.upPosition = e.pageX
-
-      const isDragRight = states.downPosition < states.upPosition
-
-      if (isDragRight) {
-        dispatch(`${storeName}/imgMove`, ['left'])
-      } else {
-        dispatch(`${storeName}/imgMove`, ['right'])
-      }
-
-      document.body.removeEventListener('mouseup', getUpPosition)
-    }
   }
+
+  function getUpPosition (e) {
+    states.upPosition = e.pageX
+
+    const isDragRight = states.downPosition < states.upPosition
+
+    if (isDragRight) {
+      dispatch(`${myStoreName}/imgMove`, ['left'])
+    } else {
+      dispatch(`${myStoreName}/imgMove`, ['right'])
+    }
+
+    document.body.removeEventListener('mouseup', getUpPosition)
+  }
+
   return {
     ...toRefs(states),
     getDownPosition
