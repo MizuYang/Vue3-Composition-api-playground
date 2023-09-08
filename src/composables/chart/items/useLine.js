1476c789366247c9db5733d348404a67cad6de62
Mizu <psdk55052004@gmail.com>
2023-09-08 15:31:44 +0800

    新增 Chart.js/Line: 第二筆資料、隨機數據

--- src/composables/chart/items/useLine.js
+++ src/composables/chart/items/useLine.js
@@ -1,15 +1,18 @@
 import { reactive, toRefs } from 'vue'
+import { useStore } from 'vuex'
 
 export function useLine () {
+  const store = useStore()
+  const { dispatch } = store
   const states = reactive({
     // 圖表數據設定
     data: {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
         {
-          label: 'Data One',
-          backgroundColor: '#f87979',
-          data: [40, 39, 10, 40, 39, 80, 40],
+          label: '第一筆資料',
+          backgroundColor: '#8F9FF3',
+          data: getRandomData(), // [40, 39, 10, 40, 30, 80, 40],
           datalabels: {
             labels: {
               title: {
@@ -17,6 +20,59 @@ export function useLine () {
                 font: { size: 15 },
                 anchor: 'start',
                 align: 'top'
+              }
+            },
+            // 事件註冊
+            listeners: {
+              click: function (context, e) {
+                if (!context.active) return
+                console.log('context', context)
+                console.log('e', e)
+
+                const index = context.dataIndex
+                const { label } = context.dataset
+                const month = states.data.labels[index]
+                const value = context.dataset.data[index]
+                const content = `
+                您點擊了 ${label} 的 ${index + 1}  月
+                ( ${month} )   ${value} 
+                `
+
+                dispatch('chart/getChartContent', content)
+              }
+            }
+          }
+        },
+        {
+          label: '第二筆資料',
+          backgroundColor: '#F59393',
+          data: getRandomData(), // [40, 39, 10, 40, 30, 80, 40],
+          datalabels: {
+            labels: {
+              title: {
+                color: '#fff',
+                font: { size: 15 },
+                anchor: 'start',
+                align: 'top'
+              }
+            },
+            // 事件註冊
+            listeners: {
+              click: function (context, e) {
+                if (!context.active) return
+                console.log('context', context)
+                console.log('e', e)
+
+                const index = context.dataIndex
+                const { label } = context.dataset
+                const month = states.data.labels[index]
+                const value = context.dataset.data[index]
+                const content = `
+                您點擊了 ${label} 的 ${index + 1}  月
+                ( ${month} )   ${value} 
+                `
+
+                dispatch('chart/getChartContent', content)
               }
             }
           }
@@ -29,6 +85,15 @@ export function useLine () {
       maintainAspectRatio: false
     }
   })
+
+  function getRandomData () {
+    const data = []
+    for (let i = 0; i <= 7; i++) {
+      data.push(Math.round(Math.random() * 100) + 1)
+    }
+
+    return data
+  }
 
   return {
     ...toRefs(states)
