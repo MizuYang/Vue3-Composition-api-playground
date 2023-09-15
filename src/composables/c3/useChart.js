f265669a3ebc2bd086a6079657f16e6f57db4dbb
Mizu <psdk55052004@gmail.com>
2023-09-15 10:54:46 +0800

    新增 c3.js: 每1.5秒改變一次圖表數據

--- src/composables/c3/useChart.js
+++ src/composables/c3/useChart.js
@@ -10,8 +10,10 @@ export function useChart () {
 
   const data = {
     columns: [
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 50, 20, 10, 40, 15, 25]
+      ['data1', ...getRandomData()],
+      ['data2', ...getRandomData()]
+      // ['data1', 30, 200, 100, 400, 150, 250],
+      // ['data2', 50, 20, 10, 40, 15, 25]
     ],
     type: 'line',
     // types: { // 同時呈現不同類型的圖表
@@ -28,14 +30,36 @@ export function useChart () {
       data2: 'data2-class'
     }
   }
+  let chart = null
 
   onMounted(() => {
   // 使用C3.js创建折线图
-    const chart = c3.generate({
+    chart = c3.generate({
       bindto: '#chart',
       data
     })
 
     commit('c3Chart/GET_CHART_ELEMENT', chart)
+
+    updateData()
   })
+
+  function getRandomData () {
+    const data = []
+    for (let i = 0; i < 6; i++) {
+      data.push(Math.round(Math.random() * 100) + 1)
+    }
+
+    return data
+  }
+  function updateData () {
+    setInterval(() => {
+      chart.load({
+        columns: [
+          ['data1', ...getRandomData()],
+          ['data2', ...getRandomData()]
+        ]
+      })
+    }, 1500)
+  }
 }
