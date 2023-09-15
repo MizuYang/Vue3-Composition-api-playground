0b570da19cb8002b0d04ac0ec5e1df48c5e247d0
Mizu <psdk55052004@gmail.com>
2023-09-15 10:20:51 +0800

    新增 c3.js: useChart.js模組

--- src/components/demo/c3/Chart.vue
+++ src/components/demo/c3/Chart.vue
@@ -3,45 +3,9 @@
 </template>
 
 <script setup>
-import { toRefs, onMounted } from 'vue'
-import { useStore } from 'vuex'
-import c3 from 'c3/c3.js'
-import 'c3/c3.css'
+import { useChart } from '@/composables/c3/useChart.js'
 
-const store = useStore()
-const { commit } = store
-const { chartType } = toRefs(store.state.c3Chart) // eslint-disable-line
-
-const data = {
-  columns: [
-    ['data1', 30, 200, 100, 400, 150, 250],
-    ['data2', 50, 20, 10, 40, 15, 25]
-  ],
-  type: 'line',
-  // types: { // 同時呈現不同類型的圖表
-  //   data1: 'bar',
-  //   data2: 'spline'
-  // },
-  colors: { // 圖表數據的顏色
-    data1: 'red',
-    data2: 'green'
-  },
-  labels: true, // 在圖表上顯示該數據的數字
-  classes: { // 為數據加上 class 方便調整樣式
-    data1: 'data1-class', // (c3-target-樣式名稱, 例:. c3-target-data1-class)
-    data2: 'data2-class'
-  }
-}
-
-onMounted(() => {
-  // 使用C3.js创建折线图
-  const chart = c3.generate({ // eslint-disable-line
-    bindto: '#chart',
-    data
-  })
-
-  commit('c3Chart/GET_CHART_ELEMENT', chart)
-})
+useChart()
 </script>
 
 <style lang='scss' scope>
---
+++ src/composables/c3/useChart.js
@@ -0,0 +1,41 @@
+
+import { onMounted } from 'vue'
+import { useStore } from 'vuex'
+import c3 from 'c3/c3.js'
+import 'c3/c3.css'
+
+export function useChart () {
+  const store = useStore()
+  const { commit } = store
+
+  const data = {
+    columns: [
+      ['data1', 30, 200, 100, 400, 150, 250],
+      ['data2', 50, 20, 10, 40, 15, 25]
+    ],
+    type: 'line',
+    // types: { // 同時呈現不同類型的圖表
+    //   data1: 'bar',
+    //   data2: 'spline'
+    // },
+    colors: { // 圖表數據的顏色
+      data1: 'red',
+      data2: 'green'
+    },
+    labels: true, // 在圖表上顯示該數據的數字
+    classes: { // 為數據加上 class 方便調整樣式
+      data1: 'data1-class', // (c3-target-樣式名稱, 例:. c3-target-data1-class)
+      data2: 'data2-class'
+    }
+  }
+
+  onMounted(() => {
+  // 使用C3.js创建折线图
+    const chart = c3.generate({
+      bindto: '#chart',
+      data
+    })
+
+    commit('c3Chart/GET_CHART_ELEMENT', chart)
+  })
+}
