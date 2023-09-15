79cac16c4fec10a59d979f33949cabd0b98b0153
Mizu <psdk55052004@gmail.com>
2023-09-15 16:18:02 +0800

    新增 vuex: modules.js模組

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,14 +1,6 @@
 import { createStore } from 'vuex'
 // import axios from 'axios'
-
-// 其它store
-import theme from '@/vuex/store/theme.js'
-import todoList from '@/vuex/store/demo/todolist/todoList.js'
-import delModal from '@/vuex/store/demo/todolist/modal/delModal.js'
-import carouselStoreA from '@/vuex/store/demo/carousel/carouselStoreA.js'
-import carouselStoreB from '@/vuex/store/demo/carousel/carouselStoreB.js'
-import chart from '@/vuex/store/demo/chart/chart.js'
-import c3Chart from '@/vuex/store/demo/c3/c3Chart.js'
+import modules from '@/vuex/store/modules.js'
 
 export default createStore({
   strict: false, // 嚴謹模式
@@ -21,12 +13,6 @@ export default createStore({
   getters: {
   },
   modules: {
-    theme,
-    todoList,
-    delModal,
-    carouselStoreA,
-    carouselStoreB,
-    chart,
-    c3Chart
+    ...modules
   }
 })
---
+++ src/vuex/store/modules.js
@@ -0,0 +1,18 @@
+// 其它store
+import theme from '@/vuex/store/theme.js'
+import todoList from '@/vuex/store/demo/todolist/todoList.js'
+import delModal from '@/vuex/store/demo/todolist/modal/delModal.js'
+import carouselStoreA from '@/vuex/store/demo/carousel/carouselStoreA.js'
+import carouselStoreB from '@/vuex/store/demo/carousel/carouselStoreB.js'
+import chart from '@/vuex/store/demo/chart/chart.js'
+import c3Chart from '@/vuex/store/demo/c3/c3Chart.js'
+
+export default {
+  theme,
+  todoList,
+  delModal,
+  carouselStoreA,
+  carouselStoreB,
+  chart,
+  c3Chart
+}
