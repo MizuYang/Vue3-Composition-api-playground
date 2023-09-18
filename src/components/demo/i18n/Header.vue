ea673394e72bb270dea01e48bb239e8e28020100
Mizu <psdk55052004@gmail.com>
2023-09-18 16:26:57 +0800

    新增 i18n: 翻譯 Header.vue

--- src/components/demo/i18n/Header.vue
+++ src/components/demo/i18n/Header.vue
@@ -1,36 +1,37 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="javascript:;">Navbar</a>
+      <a class="navbar-brand" href="javascript:;">{{ $t('NavbarBrand') }}</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="javascript:;">Home</a>
+            <a class="nav-link active" aria-current="page" href="javascript:;">{{ $t('Home')}}</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="javascript:;">Link</a>
+            <a class="nav-link" href="javascript:;">{{ $t('Link') }}</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="javascript:;" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              {{ $t('Dropdown') }}
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="javascript:;">Action</a></li>
-              <li><a class="dropdown-item" href="javascript:;">Another action</a></li>
+              <li><a class="dropdown-item" href="javascript:;">{{ $t('Action') }}</a></li>
+              <li><a class="dropdown-item" href="javascript:;">{{ $t('Anotheraction') }}</a></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="javascript:;">Something else here</a></li>
+              <li><a class="dropdown-item" href="javascript:;">{{ $t('Somethingelsehere') }}</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link disabled">Disabled</a>
+            <a class="nav-link disabled">{{ $t('Disabled') }}</a>
           </li>
         </ul>
         <form class="d-flex">
-          <input class="form-control search-input me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success search-btn" type="submit">Search</button>
+          <input class="form-control search-input me-2" type="search"
+                 :placeholder="$t('Content')" aria-label="Search">
+          <button class="btn btn-outline-success search-btn" type="submit" style="min-width:100px;">{{ $t('Search') }}</button>
         </form>
       </div>
     </div>
