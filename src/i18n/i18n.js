a5e955450a74f85e1aea818232f98518fccd20d4
Mizu <psdk55052004@gmail.com>
2023-09-18 15:35:27 +0800

    新增 i18n: 匯入i18n套件、配置、加入中英日翻譯json檔

---
+++ src/i18n/i18n.js
@@ -0,0 +1,27 @@
+import { createI18n } from 'vue-i18n'
+// 自定义国际化文件
+import zh from './lang/zh.json'
+import en from './lang/en.json'
+import ja from './lang/ja.json'
+
+// 将i18n暴露出去，在main.js中引入挂载
+export const i18n = createI18n({
+  // composition api 需設定為 false
+  legacy: false,
+
+  // 默认语言
+  locale: 'zh',
+
+  // 所選擇的語言不可用時，改用的預設語言
+  // fallbackLocale: 'en',
+
+  // 全局注册 $t
+  // globalInjection: true,
+
+  // 语言库
+  messages: {
+    zh,
+    en,
+    ja
+  }
+})
--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import store from './vuex/index'
 import VueLoading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
 
+import { i18n } from '@/i18n/i18n.js'
+
 import App from './App.vue'
 import router from './router'
 
@@ -22,6 +24,7 @@ app.component('IsLoading', VueLoading)
 
 app.use(router)
 app.use(VueAxios, axios)
+app.use(i18n)
 app.use(Vuex)
 app.use(store)
 app.mount('#app')
