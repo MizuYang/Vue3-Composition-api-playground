e9b12a422feeea9237d40b63a56a2bc7b44f23f4
Mizu <psdk55052004@gmail.com>
2023-09-18 16:26:14 +0800

    新增 i18n: 翻譯 Card.vue

--- src/components/demo/i18n/items/Card.vue
+++ src/components/demo/i18n/items/Card.vue
@@ -2,9 +2,9 @@
   <div class="card border-none">
     <img src='https://picsum.photos/400/152' class="img-fluid card-img-top" alt=''>
     <div class="card-body px-0 pb-0">
-      <h5 class="card-title p-1">Card title</h5>
-      <p class="card-text p-1">Some quick example text to build .</p>
-      <a href="javascript:;" class="btn btn-primary w-100 py-2">Go somewhere</a>
+      <h5 class="card-title p-1">{{ $t('CardTitle') }}</h5>
+      <p class="card-text p-1">{{ $t('CardContent') }}</p>
+      <a href="javascript:;" class="btn btn-primary w-100 py-2">{{ $t('Button') }}</a>
     </div>
   </div>
 </template>
