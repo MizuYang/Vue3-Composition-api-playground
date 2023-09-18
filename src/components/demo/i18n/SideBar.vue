d50839fcf94b9e215da94f2374b196a1d17087d2
Mizu <psdk55052004@gmail.com>
2023-09-18 16:16:18 +0800

    新增 i18n: 翻譯 SideBar.vue

--- src/components/demo/i18n/SideBar.vue
+++ src/components/demo/i18n/SideBar.vue
@@ -1,20 +1,45 @@
 <template>
   <div class="list-group">
-    <a href="javascript:;" class="list-group-item list-group-item-action">首頁</a>
-
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-primary">最新消息</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-secondary">產品目錄</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-success">關於我們</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-danger">聯絡我們</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-warning">常見問題</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-info">客戶評價</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-light">服務支援</a>
-    <a href="javascript:;" class="list-group-item list-group-item-action list-group-item-dark">線上訂單</a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action">
+       {{ $t('Home') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-primary">
+       {{ $t('LatestNews') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-secondary">
+       {{ $t('ProductCatalog') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-success">
+       {{ $t('AboutUs') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-danger">
+       {{ $t('ContactUs') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-warning">
+       {{ $t('FAQs') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-info">
+       {{ $t('CustomerReviews') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-light">
+       {{ $t('CustomerSupport') }}
+    </a>
+    <a href="javascript:;"
+       class="list-group-item list-group-item-action list-group-item-dark">
+       {{ $t('OnlineOrders') }}
+    </a>
   </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang='scss' scope>
